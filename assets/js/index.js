bef3d94c54926e2b615f3789f63c09f96ac67186
Your Name <you@example.com>
2018-11-12 15:59:12 -0600

    121118 Login Y agregar usuario

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,3 +2,104 @@
 $("#btnregistro").click(function(event) {
 	$("#exampleModal").modal("toggle");
 });
+
+var cont1 = document.getElementById('contrasena_nueva1');
+var cont2 = document.getElementById('confirmar_contrasena_nueva1');
+var btn_guard = document.getElementsByName('agregar_usuario')
+
+cont1.addEventListener('change',function () {
+	console.log(btn_guard);
+	if (cont1.value != cont2.value) {
+		btn_guard[0].setAttribute('disabled','disabled');
+	}else{
+		btn_guard[0].removeAttribute('disabled');
+	}
+})
+cont2.addEventListener('change',function () {
+	console.log(btn_guard);
+	if (cont1.value != cont2.value) {
+		btn_guard[0].setAttribute('disabled','disabled');
+	}else{
+		btn_guard[0].removeAttribute('disabled');
+	}
+})
+
+var cont3 = document.getElementById('contrasena_nueva2');
+var cont4 = document.getElementById('confirmar_contrasena_nueva2');
+var btn_guard1 = document.getElementsByName('editar_usuario')
+
+cont3.addEventListener('change',function () {
+	console.log(btn_guard1);
+	if (cont3.value != cont4.value) {
+		btn_guard1[0].setAttribute('disabled','disabled');
+	}else{
+		btn_guard1[0].removeAttribute('disabled');
+	}
+})
+cont4.addEventListener('change',function () {
+	console.log(btn_guard1);
+	if (cont3.value != cont4.value) {
+		btn_guard1[0].setAttribute('disabled','disabled');
+	}else{
+		btn_guard1[0].removeAttribute('disabled');
+	}
+})
+
+
+var cont5 = document.getElementById('contrasena_nueva3');
+var cont6 = document.getElementById('confirmar_contrasena_nueva3');
+var btn_guard1 = document.getElementsByName('editar_usuario')
+
+cont5.addEventListener('change',function () {
+	console.log(btn_guard1);
+	if (cont5.value != cont6.value) {
+		btn_guard1[0].setAttribute('disabled','disabled');
+	}else{
+		btn_guard1[0].removeAttribute('disabled');
+	}
+})
+cont6.addEventListener('change',function () {
+	console.log(btn_guard1);
+	if (cont5.value != cont6.value) {
+		btn_guard1[0].setAttribute('disabled','disabled');
+	}else{
+		btn_guard1[0].removeAttribute('disabled');
+	}
+})
+
+
+/*$( "#form_alumno" ).submit(function( event ) {
+	event.preventDefault();
+	console.log(event);
+	data=new Object();
+	for (var i = 0; i < event.delegateTarget.length; i++) {
+		data[event.delegateTarget[i].name]=event.delegateTarget[i].value;
+
+		if (event.delegateTarget[i].name=='foto') {
+			data[event.delegateTarget[i].name]=event.delegateTarget[i].files;
+		}
+
+	}
+	console.log(data);
+
+	$.ajax({
+		url: "http://localhost:81/proy/",
+		contentType:"multipart/form-data",
+		data:data,
+		method:"POST"
+	}).done(function() {
+		alert("Hecho enviado");
+	}).fail(function() {
+		alert( "error" );
+	}).always(function() {
+		alert( "complete" );
+	}).error(function(e) {
+		alert("Error");
+		console.log(e);
+	});
+
+
+
+  
+});
+*/
---
+++ procedimientos.php
@@ -0,0 +1,548 @@
+<?php 
+
+	/**
+	 * summary
+	 */
+	class Procedimientos
+	{
+	    /**
+	     * summary
+	     */
+	    private $con=null;
+	    
+	    public function __construct(){
+	        $con=$this->conexion_mysql();
+	        if ($con!=null) {
+				echo "<h1>Conectado</h1><br>";
+	        }
+	        $con->close();
+
+	    }
+
+	    private function conexion_mysql(){
+	    	$mysqli = new mysqli("127.0.0.1", "root", "", "escompartiendo", 3306);
+
+			if ($mysqli->connect_errno) {
+			    echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+			    return null;
+			}
+
+			return $mysqli;
+	    }
+
+	    public function insertar_permiso($descripcion=""){
+
+			$con=$this->conexion_mysql();
+			if ($con!=null) {
+				
+
+					$result=mysqli_query($con,"INSERT INTO permiso(descripcion) values ('$descripcion');");
+
+					$return = $result;
+
+					$con->close();
+
+					return $return;				
+
+				
+				
+			}else {
+
+
+				echo mysqli_error($con);				
+				return false;
+
+			}
+
+		}
+
+		public function insertar_rol($descripcion=""){
+
+			$con=$this->conexion_mysql();
+
+
+			if ($con!=null) {
+
+				
+					$result=mysqli_query($con,"INSERT INTO rol(descripcion) values ('$descripcion');");
+					if ($result) {
+						$result=mysqli_query($con,"SELECT * FROM rol WHERE descripcion='$descripcion'");
+
+						$i=0;
+						
+						while ($fila = $result->fetch_row()) {
+						    $return[$i]=$fila;
+						    $i++;
+						}
+
+						$result->close();
+					}else {
+						
+						$return = $result;
+
+						$result->close();
+						
+					}
+
+					$con->close();
+
+					return $return;
+
+							
+				
+			}else {
+
+
+				echo mysqli_error($con);
+				return false;
+				
+			}
+
+		}
+
+		public function agregar_permiso_rol($rol="",$permiso=""){
+			$con=$this->conexion_mysql();
+			if ($con!=null) {
+				
+					$result=mysqli_query($con,"INSERT INTO rol_permiso(rol,permiso) values ($rol,$permiso);");
+
+					$return = $result;
+
+					$con->close();
+
+					return $return;
+
+				
+
+				
+				
+			}else {
+
+
+				echo mysqli_error($con);
+				return false;
+				
+			}
+
+		}
+
+		public function insertar_usuario($correo,$contraseña,$nombre,$paterno,$materno,$fecha_nacimiento,$boleta,$curp,$status,$rol){
+
+			$con=$this->conexion_mysql();
+			if ($con!=null) {
+					
+					$result=mysqli_query($con,"INSERT INTO usuario(correo,contrasena,fecha_alta,nombre,paterno,materno,fecha_nacimiento,boleta,CURP,status,rol,foto) values ('$correo','$contraseña',CURDATE(),'$nombre','$paterno','$materno','$fecha_nacimiento','$boleta','$curp',$status,$rol,'')");
+					
+					if ($result) {
+						$result=mysqli_query($con,"SELECT * FROM usuario WHERE correo='$correo'");
+
+						$i=0;
+						
+						while ($fila = $result->fetch_row()) {
+						    $return[$i]=$fila;
+						    $i++;
+						}
+
+						$result->close();
+					}else {
+						
+						$return = $result;
+						
+					}
+
+					echo mysqli_error($con);
+
+					$con->close();
+
+					return $return;									
+				
+			}else {
+
+
+				echo mysqli_error($con);
+				return false;
+				
+			}
+
+		}
+
+		public function insertar_foto_usuario($foto_path,$correo){
+
+			$con=$this->conexion_mysql();
+			if ($con!=null) {
+				
+					$result=mysqli_query($con,"UPDATE usuario SET foto='$foto_path' WHERE correo='$correo'");
+
+					$return = $result;
+
+					$con->close();
+
+					return $return;		
+		
+				
+			}else {
+
+
+				echo mysqli_error($con);
+				return false;
+				
+			}
+
+		}
+
+		public function seleccionar_permiso(){
+			$con= $this->conexion_mysql();
+			if ($con!=null) {
+				
+					if($result=mysqli_query($con,"SELECT * FROM permiso")){
+						
+						$i=0;
+						
+						while ($fila = $result->fetch_row()) {
+						    $return[$i]=$fila;
+						    $i++;
+						}
+
+						$result->close();
+					}else {
+						echo mysqli_error($con);
+						$return = $result;
+						
+					}
+
+					$con->close();
+
+					return $return;
+					
+				
+			}
+			else{
+				echo mysqli_error($con);
+				return false;
+
+			}
+			
+		}
+
+		public function seleccionar_rol(){
+			$con= $this->conexion_mysql();
+			if ($con!=null) {
+				
+					if($result=mysqli_query($con,"SELECT * FROM rol")){
+						
+						$i=0;
+						
+						while ($fila = $result->fetch_row()) {
+						    $return[$i]=$fila;
+						    $i++;
+						}
+
+						$result->close();
+					}else {
+						echo mysqli_error($con);
+						$return = $result;
+						
+					}
+
+					$con->close();
+
+					return $return;
+					
+				
+			}
+			else{
+				echo mysqli_error($con);
+				return false;
+
+			}
+			
+		}
+
+		public function seleccionar_usuarios(){
+			$con= $this->conexion_mysql();
+			if ($con!=null) {
+				
+					if($result=mysqli_query($con,"SELECT * FROM usuario")){
+						
+						$i=0;
+						
+						while ($fila = $result->fetch_row()) {
+						    $return[$i]=$fila;
+						    $i++;
+						}
+
+						$result->close();
+					}else {
+						echo mysqli_error($con);
+						$return = $result;
+						
+					}
+
+					$con->close();
+
+					return $return;
+					
+				
+			}
+			else{
+				echo mysqli_error($con);
+				return false;
+
+			}
+			
+		}
+
+		public function seleccionar_usuario($id_usuario){
+			$con= $this->conexion_mysql();
+
+			if ($con!=null) {
+				
+					if($result=mysqli_query($con,"SELECT * FROM usuario WHERE id=$id_usuario")){
+						
+						$i=0;
+						
+						while ($fila = $result->fetch_row()) {
+						    $return[$i]=$fila;
+						    $i++;
+						}
+
+						$result->close();
+					}else {
+
+						echo mysqli_error($con);
+
+						$return = $result;
+						
+					}
+
+					$con->close();
+
+					return $return;
+					
+				
+			}
+			else{
+				echo(mysqli_error($con));
+				return false;
+
+			}
+			
+		}
+
+		public function editar_usuario($correo,$contraseña,$nombre,$paterno,$materno,$fecha_nacimiento,$boleta,$curp,$status,$rol){
+
+			$con=$this->conexion_mysql();
+			if ($con!=null) {
+					
+					$result=mysqli_query($con," UPDATE usuario SET contrasena='$contraseña',nombre='$nombre', paterno='$paterno' , materno='$materno',fecha_nacimiento='$fecha_nacimiento' ,boleta='$boleta' ,CURP='$curp',rol=$rol WHERE correo='$correo'");
+					
+					if ($result) {
+						$result=mysqli_query($con,"SELECT * FROM usuario WHERE correo='$correo'");
+
+						$i=0;
+						
+						while ($fila = $result->fetch_row()) {
+						    $return[$i]=$fila;
+						    $i++;
+						}
+
+						$result->close();
+					}else {
+						
+						$return = $result;
+						
+					}
+
+					$con->close();
+
+					return $return;									
+				
+			}else {
+
+
+				echo mysqli_error($con);
+				return false;
+				
+			}
+
+		}
+
+		public function borrar_usuario($id){
+			$con=$this->conexion_mysql();
+			if ($con!=null) {
+
+				
+
+					$result=mysqli_query($con,"DELETE FROM usuario WHERE id=$id");
+
+					$return = $result;
+
+					$con->close();
+
+					return $return;		
+				
+				
+			}else {
+
+
+				echo mysqli_error($con);				
+				return false;
+
+			}
+		}
+
+		public function agregar_ua($nombre, $descripcion, $nivel){
+
+			$con=$this->conexion_mysql();
+			if ($con!=null) {
+				
+
+					$result=mysqli_query($con,"INSERT INTO unidad_aprendizaje(nombre,descripcion,nivel) values ('$nombre','$descripcion',nivel);");
+
+					$return = $result;
+
+					echo mysqli_error($con);
+
+					$con->close();				
+
+					return $return;				
+
+				
+				
+			}else {
+
+
+				echo mysqli_error($con);				
+				return false;
+
+			}
+
+		}
+
+		public function seleccionar_profesores(){
+
+			$con= $this->conexion_mysql();
+			if ($con!=null) {
+				
+					if($result=mysqli_query($con,"SELECT * FROM usuario WHERE rol = (SELECT id FROM rol WHERE descripcion='profesor')")){
+						
+						$i=0;
+						
+						while ($fila = $result->fetch_row()) {
+						    $return[$i]=$fila;
+						    $i++;
+						}
+
+						$result->close();
+					}else {
+						echo mysqli_error($con);
+						$return = $result;
+						
+					}
+
+					$con->close();
+
+					return $return;
+					
+				
+			}
+			else{
+				echo mysqli_error($con);
+				return false;
+
+			}
+
+		}
+
+		public function seleccionar_uas(){
+			$con= $this->conexion_mysql();
+			if ($con!=null) {
+				
+					if($result=mysqli_query($con,"SELECT * FROM unidad_aprendizaje")){
+						
+						$i=0;
+						
+						while ($fila = $result->fetch_row()) {
+						    $return[$i]=$fila;
+						    $i++;
+						}
+
+						$result->close();
+					}else {
+						echo mysqli_error($con);
+						$return = $result;
+						
+					}
+
+					$con->close();
+
+					return $return;
+					
+				
+			}
+			else{
+				echo mysqli_error($con);
+				return false;
+
+			}
+			
+		}
+
+		public function asignar_ua_profesor($profesor, $ua){
+
+			$con=$this->conexion_mysql();
+			if ($con!=null) {
+				
+
+					$result=mysqli_query($con,"INSERT INTO ua_profesor(ua,usuario) values ($ua,$profesor);");
+
+					$return = $result;
+
+					echo mysqli_error($con);
+
+					$con->close();				
+
+					return $return;				
+
+				
+				
+			}else {
+
+
+				echo mysqli_error($con);				
+				return false;
+
+			}
+
+		}
+		public function validar_usuario($usuario, $contrasena){
+
+			$con=$this->conexion_mysql();
+			if ($con!=null) {
+				
+
+					$result=mysqli_query($con,"SELECT * FROM usuario WHERE correo='$usuario' and contrasena='$contrasena';");
+
+					$return = isset($result);
+
+					echo mysqli_error($con);
+
+					$con->close();				
+
+					return $return;				
+
+				
+				
+			}else {
+
+
+				echo mysqli_error($con);				
+				return false;
+
+			}
+
+		}
+	}	
+
+
+?>
